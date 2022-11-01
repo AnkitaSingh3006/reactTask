94ea3e6338d5420f366ffc0a309132b3bbb1a7ee
Ankita Singh <anki3404@gmail.com>
2022-11-01 17:00:59 +0530

    code push

--- task/src/component/queryString.js
+++ task/src/component/queryString.js
@@ -1,18 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
 import {useSearchParams} from 'react-router-dom'
+import './useParams.css'
 
 function SearchUser() {
 
-  const [searchParams] = useSearchParams();
+  const [searchParams, setSearchParams] = useSearchParams();
   console.warn(searchParams.get('age'))
   console.warn(searchParams.get('city'))
   const age = searchParams.get('age')
   const city = searchParams.get('city')
 
+  useEffect(()=>{
+    console.log(age,city)
+  },[age,city])
+
   return (
     <div>
       <h1>Age is:{age}</h1>
       <h1>city is:{city}</h1>
+      <button className="btns" onClick={() => setSearchParams({age:'47',city:'vns'})}>Change!</button>
     </div>
   )
 }
