ac2880c71e34be73d8a85bed90fbbbf4281b6130
Ankita Singh <anki3404@gmail.com>
2022-11-14 16:27:09 +0530

    code push

--- task/src/component/userData.js
+++ task/src/component/userData.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import './userData.css';
 import axios from "axios";
-import DataTable from 'react-data-table-component'
+// import DataTable from 'react-data-table-component'
 
 function Userdata() {
 
@@ -10,8 +10,8 @@ function Userdata() {
     const getUserList = async () => {
         try {
             const response = await axios.get("https://reqres.in/api/users?page=2");
-            setUserlist(response.data);
-            console.log("API",response.data)
+            setUserlist(response.data.data);
+            console.log("API", response.data)
         } catch (error) {
             console.log(error);
         }
@@ -19,28 +19,28 @@ function Userdata() {
 
     // console.log("userList",userlist)
 
-    const columns = [
-        {
-            name: "id",
-            selector: (row) => row.id,
-        },
-        {
-            name: "email",
-            selector: (row) => row.email,
-        },
-        {
-            name: "firstname",
-            selector: (row) => row.first_name,
-        },
-        {
-            name: "lastname",
-            selector: (row) => row.last_name,
-        },
-        {
-            name: "avatar",
-            selector: (row) => row.avatar,
-        },
-    ]
+    // const columns = [
+    //     {
+    //         name: "id",
+    //         selector: (row) => row.id,
+    //     },
+    //     {
+    //         name: "email",
+    //         selector: (row) => row.email,
+    //     },
+    //     {
+    //         name: "firstname",
+    //         selector: (row) => row.first_name,
+    //     },
+    //     {
+    //         name: "lastname",
+    //         selector: (row) => row.last_name,
+    //     },
+    //     {
+    //         name: "avatar",
+    //         selector: (row) => <img width={95} height={95} src={row.avatar} alt="loading" />,
+    //     },
+    // ]
 
     useEffect(() => {
         getUserList();
@@ -48,7 +48,28 @@ function Userdata() {
 
     return (
         <div className="userContainer">
-            <DataTable columns={columns} data={userlist} />
+            {/* <DataTable columns={columns} data={userlist} /> */}
+
+            <table>
+                <tr>
+                    <th>Id</th>
+                    <th>Email</th>
+                    <th>First Name</th>
+                    <th>Last Name</th>
+                    <th>Avatar</th>
+                </tr>
+                {userlist.map((val, key) => {
+                    return (
+                        <tr key={key}>
+                            <td>{val.id}</td>
+                            <td>{val.email}</td>
+                            <td>{val.first_name}</td>
+                            <td>{val.last_name}</td>
+                            <td><img width={95} height={95} src={val.avatar} alt="loading" /></td>
+                        </tr>
+                    )
+                })}
+            </table>
         </div>
     )
 }
